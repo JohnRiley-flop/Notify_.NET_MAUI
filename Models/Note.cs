8f65449a270c51b1cefdf949a94439d6031f34b5
JohnRiley-flop <jriley71904@gmail.com>
2024-10-12 22:33:01 -0500

    Added CollectionView widget and model for Note entries with a few fields.

---
+++ Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notify.Models
+{
+    public class Note
+    {
+        public int ID { get; set; }
+        public required string Title { get; set; }
+        public required string Entry { get; set; }
+
+    }
+}
