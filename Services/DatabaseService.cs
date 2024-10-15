067b914339bc964c46a2cf0ea95ac05c7d4be332
JohnRiley-flop <jriley71904@gmail.com>
2024-10-15 13:06:24 -0500

    Began working on process to add an item to the list on the backend side. :zap:

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Notify.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Notify.Services
 {
     class DatabaseService
     {
+        public async static Task AddNoteAsync(Note NewNote)
+        {
+
+        }
     }
 }
--- ViewModels/MainPageViewModel.cs
+++ ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Notify.Models;
+using Notify.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,9 +17,12 @@ namespace Notify.ViewModels
 
         //Properties
         public ObservableCollection<Note> ShowingEntries { get; set; }
+        //Commands
+        public Command AddNewEntryCommand { get; set; }
 
         public MainPageViewModel()
         {
+            //Displaying List of Entries
             ShowingEntries =
             [
                 new Note { ID=1, Title="Entry", Entry="Fill"},
@@ -34,10 +38,25 @@ namespace Notify.ViewModels
                 new Note { ID=1, Title="Entry", Entry="Fill"},
             ];
 
+            //Commands to link front and back
+            AddNewEntryCommand = new (AddNewEntry);
+
 
         }
 
+        private async void AddNewEntry()
+        {
+            //Note toAdd = NAVIGATE TO ADD PAGE AND RETURN NEW NOTE ARGUMENTS
+            Note toAdd = new() { ID=1, Title="Note", Entry="Fill" };
+            if (null == toAdd) { return; }
+            await DatabaseService.AddNoteAsync(toAdd);
+            //Reload Page because a change was made
+        }
 
+        private void UpdateList()
+        {
+
+        }
 
         public void OnPropertyChanged([CallerMemberName] string name = "") =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
